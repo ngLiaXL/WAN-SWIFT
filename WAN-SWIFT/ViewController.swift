a72bcaaae4470abc4940be3000ffd8666f28595e
ngliaxl <ngliaxl@gmail.com>
2020-11-10 15:21:15 +0800

    INIT SWIFT PROJET

---
+++ WAN-SWIFT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WAN-SWIFT
+//
+//  Created by XiangLong.Liang on 2020/11/10.
+//  Copyright © 2020 XiangLong.Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
